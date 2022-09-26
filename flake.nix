c62133258373aaf51f3dc8b8f87d631d82085120
Bernardo Meurer <bernardo@meurer.org>
2022-09-26 15:35:05 -0300

    refactor: use pre-commit-config.yaml

--- flake.nix
+++ flake.nix
@@ -2,18 +2,24 @@
   description = "lovesegfault's CV";
 
   inputs = {
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    pre-commit = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, gitignore, nixpkgs, utils }:
-    utils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ] (system:
+  outputs = { self, flake-utils, gitignore, nixpkgs, pre-commit }:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ gitignore.overlay ]; };
+        inherit (pkgs) lib;
       in
       {
         packages.default = pkgs.stdenv.mkDerivation {
@@ -52,13 +58,37 @@
         };
 
         devShells.default = self.packages.${system}.default.overrideAttrs (oldAttrs: {
-          buildInputs = with pkgs; (oldAttrs.buildInputs or [ ]) ++ [
+          nativeBuildInputs = with pkgs; (oldAttrs.nativeBuildInputs or [ ]) ++ [
             ltex-ls
-            nix-linter
             nixpkgs-fmt
-            pre-commit
             texlab
           ];
+
+          inherit (self.checks.${system}.pre-commit) shellHook;
         });
+
+        checks.pre-commit = pre-commit.lib.${system}.run {
+          src = pkgs.gitignoreSource ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+            actionlint = {
+              enable = true;
+              files = "^.github/workflows/";
+              types = [ "file" "yaml" ];
+              entry = lib.getExe pkgs.actionlint;
+            };
+            chktex = {
+              enable = true;
+              types = [ "file" "tex" ];
+              entry = "${pkgs.texlive.combined.scheme-medium}/bin/chktex";
+            };
+            latexindent = {
+              enable = true;
+              types = [ "file" "tex" ];
+              entry = "${pkgs.texlive.combined.scheme-medium}/bin/latexindent --local --silent --modifyIfDifferent";
+            };
+          };
+        };
       });
 }
